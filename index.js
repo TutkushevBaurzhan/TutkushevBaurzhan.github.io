371cfdace233691f82320972c0af381e1af9942e
TutkushevBaurzhan <59994036+TutkushevBaurzhan@users.noreply.github.com>
2020-04-04 22:35:51 +0600

    put the code in order
    put the code in order

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Declaration of required variables
 let
     canv = document.getElementById('canv'),
     ctx = canv.getContext('2d'), 
@@ -31,10 +32,15 @@ canv.width = window.innerWidth;
 canv.height = window.innerHeight;
 
 
+
+
+
+
+// Adding event listeners
 mc.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
 document.addEventListener('keypress', keypressHandler);
 
-// Свайп вниз
+// SWIPE вниз
 function swipedownHandler() {
 
     if(lastDirection !== 'up') {
@@ -52,7 +58,7 @@ function swipedownHandler() {
 }
 mc.on('swipedown',swipedownHandler) 
 
-// Свайп вверх
+// SWIPE UP
 function swipeupHandler() {
     if(lastDirection !== 'down') {
         if(pressTimes === 0) {
@@ -69,7 +75,7 @@ function swipeupHandler() {
 }
 mc.on('swipeup', swipeupHandler)
 
-// Свайп вправо
+// SWIPE RIGHT
 function swiperightHandler() {
     if(lastDirection !== 'left') {    
         if(pressTimes === 0) {
@@ -87,7 +93,7 @@ function swiperightHandler() {
 }
 mc.on('swiperight', swiperightHandler)
 
-// Свайп влево 
+// SWIPE LEFT 
 function swipeleftHandler() {
 
     if(lastDirection !== 'right') {        
@@ -140,7 +146,7 @@ function keypressHandler(e) {
         dy = 0;
 
         setTimeout(() => {lastDirection = 'left'}, interval);
-        // pressTimes++;
+    
     }
     // UP
     if((e.keyCode === 119 || e.keyCode === 1094) && lastDirection !== 'down') {
@@ -157,6 +163,121 @@ function keypressHandler(e) {
         
     }
 };
+
+
+
+
+
+function appleCordsGeneration() {
+    let x, y,
+        appleNewCords,
+        snakeCopy;
+    while(true) {
+        x = randomInteger(cellSize, canv.width-cellSize*2);
+        y = randomInteger(cellSize, canv.height-cellSize*2);
+        if(x % cellSize === 0 && y % cellSize === 0) {
+            snakeCopy = arrStringify(snake.slice());
+            appleNewCords = {x: x, y: y};
+            if(snakeCopy.includes(JSON.stringify(appleNewCords))) {
+                continue;
+            } else {
+                return appleNewCords;
+            }
+        }
+    }
+};
+
+    
+
+
+// GAME PROCESS
+inputForScore.innerText = score;
+drawSnake();
+drawApple(appleCords, appleSize);
+function gameProcess() {
+    canv.width = window.innerWidth;
+    canv.height = window.innerHeight;
+    
+    inputForScore.innerText = score;
+    let snakeCopy,
+        snakeNewSegmentX,
+        snakeNewSegmentY;
+    if(status === 'start') {
+            inputForScore.value = score;
+            ctx.clearRect(0, 0, canv.width, canv.height);
+            drawSnake();
+            drawApple(appleCords, appleSize);
+            // CHECKING FOR COLLISION WITH A WALL
+            if(snake[snake.length-1].x + cellSize > canv.width || snake[snake.length-1].y > canv.height - cellSize || snake[snake.length-1].x + cellSize < 0+cellSize || snake[snake.length-1].y < 0) {
+                gameOver();
+                }
+            snakeCopy = arrStringify(snake.slice(0, snake.length-1));
+            // CHECK FOR COLLISION WITH ITSELF
+            if(snakeCopy.includes(JSON.stringify(snake[snake.length-1]))) {
+                    gameOver();
+                   }
+            snake.shift();
+            snake[snake.length] = {x: snake[snake.length-1].x + dx, y: snake[snake.length-1].y + dy};
+            // CHECKING FOR EATING AN APPLE
+            if(snake[snake.length-1].x === appleCords.x && snake[snake.length-1].y === appleCords.y) {                                
+                snakeNewSegmentX = snake[0].x - dx;
+                snakeNewSegmentX = snake[0].y - dy;
+                snake.unshift({snakeNewSegmentX, snakeNewSegmentY});
+                appleCords = appleCordsGeneration();
+                score += 15;
+                ctx.clearRect(appleCords.x, appleCords.y, appleSize, appleSize);
+                }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Other functions
+
+
+
+function arrStringify(arr) {
+    arr.forEach((element, index) => {
+        arr[index] = JSON.stringify(element);
+    });
+    return arr;
+}
+
+
+
+
+
+function gameOver() {
+    status = 'lose';
+    document.removeEventListener('keypress', keypressHandler);
+    mc.off('swipedown', swipedownHandler);    
+    mc.off('swipeup', swipeupHandler);
+    mc.off('swiperight', swiperightHandler);
+    mc.off('swipeleft', swipeleftHandler);    
+    ctx.clearRect(0, 0, canv.width, canv.height);
+
+    // Перезапуск игры
+    if(confirm('Вы проиграли. Начать заново?')) {
+        location.reload();
+    } else {
+        alert('Пока');
+    }
+}
+function randomInteger(min, max) {
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+}
+
 
 
 
@@ -182,95 +303,4 @@ function drawApple(cords, size) {
     ctx.rect(cords.x, cords.y, size, size);
     ctx.fill();
     ctx.closePath();
-}
-
-function appleCordsGeneration() {
-    let x, y,
-        appleNewCords,
-        snakeCopy;
-    while(true) {
-        x = randomInteger(cellSize, canv.width-cellSize*2);
-        y = randomInteger(cellSize, canv.height-cellSize*2);
-        if(x % cellSize === 0 && y % cellSize === 0) {
-            snakeCopy = arrStringify(snake.slice());
-            appleNewCords = {x: x, y: y};
-            if(snakeCopy.includes(JSON.stringify(appleNewCords))) {
-                continue;
-            } else {
-                return appleNewCords;
-            }
-        }
-    }
-};
-
-    
-
-
-
-inputForScore.innerText = score;
-drawSnake();
-drawApple(appleCords, appleSize);
-function gameProcess() {
-    canv.width = window.innerWidth;
-    canv.height = window.innerHeight;
-    
-    inputForScore.innerText = score;
-    let snakeCopy,
-        snakeNewSegmentX,
-        snakeNewSegmentY;
-    if(status === 'start') {
-            inputForScore.value = score;
-            ctx.clearRect(0, 0, canv.width, canv.height);
-            drawSnake();
-            drawApple(appleCords, appleSize);
-            // Проверка на столкновение со стеной
-            if(snake[snake.length-1].x + cellSize > canv.width || snake[snake.length-1].y > canv.height - cellSize || snake[snake.length-1].x + cellSize < 0+cellSize || snake[snake.length-1].y < 0) {
-                gameOver();
-                }
-            snakeCopy = arrStringify(snake.slice(0, snake.length-1));
-            // Проверка на столкновение с собой
-            if(snakeCopy.includes(JSON.stringify(snake[snake.length-1]))) {
-                    gameOver();
-                   }
-            snake.shift();
-            snake[snake.length] = {x: snake[snake.length-1].x + dx, y: snake[snake.length-1].y + dy};
-            // Проверка на съедение яблока
-            if(snake[snake.length-1].x === appleCords.x && snake[snake.length-1].y === appleCords.y) {                                
-                snakeNewSegmentX = snake[0].x - dx;
-                snakeNewSegmentX = snake[0].y - dy;
-                snake.unshift({snakeNewSegmentX, snakeNewSegmentY});
-                appleCords = appleCordsGeneration();
-                score += 15;
-                ctx.clearRect(appleCords.x, appleCords.y, appleSize, appleSize);
-                }
-    }
-};
-
-
-function arrStringify(arr) {
-    arr.forEach((element, index) => {
-        arr[index] = JSON.stringify(element);
-    });
-    return arr;
-}
-
-function gameOver() {
-    status = 'lose';
-    document.removeEventListener('keypress', keypressHandler);
-    mc.off('swipedown', swipedownHandler);    
-    mc.off('swipeup', swipeupHandler);
-    mc.off('swiperight', swiperightHandler);
-    mc.off('swipeleft', swipeleftHandler);    
-    ctx.clearRect(0, 0, canv.width, canv.height);
-
-    // Перезапуск игры
-    if(confirm('Вы проиграли. Начать заново?')) {
-        location.reload();
-    } else {
-        alert('Пока');
-    }
-}
-function randomInteger(min, max) {
-    let rand = min - 0.5 + Math.random() * (max - min + 1);
-    return Math.round(rand);
 }
